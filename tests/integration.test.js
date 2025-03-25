6220a0dc549b25c03d8b3cc9a170ac2a4239a83f
Shivansh Singhania <136645997+Shivansh-08@users.noreply.github.com>
2025-03-25 15:12:55 +0530

    Update integration.test.js

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,6 +1,10 @@
 const request = require('supertest');
 
-const BASE_URL = 'http://localhost';
+const BASE_URL = 'http://localhost:80';
+
+beforeAll(async () => {
+    await new Promise(resolve => setTimeout(resolve, 5000)); // Wait for Nginx
+});
 
 test('GET / should return Hello from Docker', async () => {
     const res = await request(BASE_URL).get('/');
